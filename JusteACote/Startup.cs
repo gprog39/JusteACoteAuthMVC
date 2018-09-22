9889974b0709904104f4bdf58adb93ae33651107
gprog39 <pyfes@aol.com>
2018-09-22 19:09:43 +0200

    Ajoutez des fichiers projet.

---
+++ JusteACote/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JusteACote.Startup))]
+namespace JusteACote
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
